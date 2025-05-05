a7054994bb7f847479642aa4c3a5a2c4309cb668
Nafiseh <nafisehz842@gmail.com>
2025-05-05 12:59:49 +0330

    dasham

--- src/main/java/Model/Map/Coordinate.java
+++ src/main/java/Model/Map/Coordinate.java
@@ -1,6 +1,22 @@
 package Model.Map;
 
 public class Coordinate {
-    int x;
-    int y;
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }

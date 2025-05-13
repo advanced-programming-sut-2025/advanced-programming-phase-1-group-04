475561d06ed0330ee59db525d1f3dbf38165f38d
aynaz <aynazrahmani.1385@gmail.com>
2025-05-13 21:44:15 +0330

    shash

--- src/main/java/Controller/SirkBozorg/PlantController.java
+++ src/main/java/Controller/SirkBozorg/PlantController.java
@@ -15,10 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PlantController {
-    public void lightning () {
-
-    }
-
+    
     public static Result craftInfo (String craftName) {
         if (App.getCurrentGame().getCurrentPlayer().getMovesThisTurn() >= App.getCurrentGame().getCurrentPlayer().getMaxMovesInTurn()) {
             return new Result (false, "you have no more moves! enter next turn!");
